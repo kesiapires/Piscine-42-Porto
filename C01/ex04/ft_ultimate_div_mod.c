eef6549a190e092d77f7ea3b1fb85421372c703f
Kesia Pires <kesiapiressantos@gmail.com>
2024-10-21 01:36:45 +0100

    pointers, same as the previous one but needed a temporary variable so the value (a and b) won't change, and then the operation can work

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: k-pires  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 10:53:23 by k-pires           #+#    #+#             */
+/*   Updated: 2024/10/18 10:59:31 by k-pires          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	temp_a;
+	int	temp_b;
+
+	temp_a = *a;
+	temp_b = *b;
+	*a = temp_a / temp_b;
+	*b = temp_a % temp_b;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 5;
+	b = 2;
+	ft_ultimate_div_mod(&a, &b);
+	printf("div: %d mod: %d\n", a, b);
+	return (0);
+}

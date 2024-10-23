19cc68329d27cf658503219173bb0fbbcc13fcab
Kesia Pires <kesiapiressantos@gmail.com>
2024-10-23 10:54:07 +0100

    exercise list and strcpy function

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: k-pires  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 10:53:23 by k-pires           #+#    #+#             */
+/*   Updated: 2024/10/18 10:59:31 by k-pires          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+int	main(void)
+{
+	char	text1[20] = "rainbow";
+	char	text2[20] = "buiatchaka";
+
+	ft_strcpy(text1, text2);
+	printf("%s %s\n", text1, text2);
+	return (0);
+}

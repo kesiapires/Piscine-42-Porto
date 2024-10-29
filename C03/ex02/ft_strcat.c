62e2d071be2793690f0d81094ba9c1d6bf0cc6f8
Kesia Pires <kesiapiressantos@gmail.com>
2024-10-29 23:29:53 +0000

    ft_strcat

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: k-pires  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 10:53:23 by k-pires           #+#    #+#             */
+/*   Updated: 2024/10/18 10:59:31 by k-pires          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i = 0;
+	int	len_dest = 0;
+
+	while (dest[len_dest] != '\0')
+	{
+		len_dest++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[len_dest + i] = src[i];
+		i++;
+	}
+	dest[len_dest + i] = '\0';
+	return (dest);
+}
+int	main(void)
+{
+	char	text1[] = "banana";
+	char	text2[] = "strawberry";
+
+	printf("%s\n", ft_strcat(text1, text2));
+	return (0);
+}

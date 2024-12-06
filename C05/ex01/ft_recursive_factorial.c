3f43ac2caf14ca6cd65d53c2b5c40c63be34e892
Kesia Pires <kesiapiressantos@gmail.com>
2024-12-06 16:32:25 +0000

    ft_recursive_factorial

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: k-pires  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 10:53:23 by k-pires           #+#    #+#             */
+/*   Updated: 2024/10/18 10:59:31 by k-pires          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_recursive_factorial(int nb)
+{
+	int	i;
+
+	i = nb;
+	if (nb < 0)
+	{
+		return (0);
+	}
+	if (nb == 0 || nb == 1)
+	{
+		return (1);
+	}
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+int	main(void)
+{
+	printf("%i\n", ft_recursive_factorial(5));
+	return (0);
+}

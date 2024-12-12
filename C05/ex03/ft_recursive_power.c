efab8771f04f01ec99b614ec812fec0b20512a83
Kesia Pires <kesiapiressantos@gmail.com>
2024-12-12 13:44:30 +0000

    ft_recursive_power

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: k-pires  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 10:53:23 by k-pires           #+#    #+#             */
+/*   Updated: 2024/10/18 10:59:31 by k-pires          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power > 1)
+	{
+		nb *= ft_recursive_power(nb, power-1);
+	}
+	return (nb);
+}
+int	main(void)
+{
+	printf("%i\n", ft_recursive_power(2, 3));
+	return (0);
+}
